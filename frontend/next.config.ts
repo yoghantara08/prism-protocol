d8738ded251aa6ba296bf9baac93b565b2461841
yoghantara08 <gyoghantara@gmail.com>
2025-12-01 22:12:17 +0800

    update nextjs version

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  // reactStrictMode: true,
   // webpack: (config) => {
   //   config.externals.push("pino-pretty", "lokijs", "encoding");
   //   return config;
