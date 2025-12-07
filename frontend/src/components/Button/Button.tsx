ed6a98b0350f347cc7949503d287728e67befd4d
yoghantara08 <gyoghantara@gmail.com>
2025-12-07 22:54:00 +0800

    adjust button rounded

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -29,7 +29,7 @@ const Button = ({
       onClick={!disabled ? onClick : undefined}
       disabled={disabled}
       className={classNames(
-        "inline-flex items-center justify-center rounded-xl px-4 py-2.5",
+        "inline-flex items-center justify-center rounded-3xl px-4 py-2.5",
         "text-sm font-medium transition-colors focus:outline-none md:text-base",
 
         {
