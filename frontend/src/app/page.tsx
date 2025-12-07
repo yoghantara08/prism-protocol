5f642af55964e0d3f24a560b2397a48eabdde79a
yoghantara08 <gyoghantara@gmail.com>
2025-12-07 22:16:08 +0800

    theme config, Button component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-
+import Button from "@/components/Button/Button";
 import PageLayout from "@/components/Layout/PageLayout";
 
 const App = () => {
   return (
     <PageLayout>
-      Prism Protocol
-      <ConnectButton />
+      <div className="m-10 space-x-3">
+        <Button className="w-40">Approve</Button>
+        <Button variant="secondary" className="w-40">
+          Swap
+        </Button>
+      </div>
     </PageLayout>
   );
 };
---
+++ frontend/src/components/Button/Button.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+import classNames from "classnames";
+
+export interface ButtonProps {
+  id?: string;
+  children: React.ReactNode;
+  className?: string;
+  disabled?: boolean;
+  // eslint-disable-next-line no-unused-vars
+  onClick?: (() => void) | ((e: any) => void);
+  variant?: "primary" | "secondary";
+  type?: "button" | "submit" | "reset";
+}
+
+const Button = ({
+  id,
+  children,
+  className,
+  onClick,
+  disabled = false,
+  variant = "primary",
+  type = "button",
+}: ButtonProps) => {
+  return (
+    <button
+      id={id}
+      type={type}
+      onClick={!disabled ? onClick : undefined}
+      disabled={disabled}
+      className={classNames(
+        "inline-flex items-center justify-center rounded-xl px-4 py-2.5",
+        "text-sm font-medium transition-colors focus:outline-none md:text-base",
+
+        {
+          // Primary — Accent system
+          "bg-accent-muted text-accent hover:bg-accent-glow cursor-pointer":
+            variant === "primary" && !disabled,
+          "bg-accent-muted text-accent-text cursor-not-allowed opacity-50":
+            variant === "primary" && disabled,
+
+          // Secondary — Neutral/system
+          "bg-background text-primary hover:bg-surface hover:border-surface cursor-pointer border":
+            variant === "secondary" && !disabled,
+          "bg-surface text-muted cursor-not-allowed":
+            variant === "secondary" && disabled,
+        },
+
+        className,
+      )}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
